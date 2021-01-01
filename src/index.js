63969de8bb4ee7fe21ce5faf10f2474afbaa43bf
Sana Saeed <41577469+sanaasaeed@users.noreply.github.com>
2021-01-02 00:53:22 +0500

    camera angle changed

--- src/index.js
+++ src/index.js
@@ -5,21 +5,23 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.getElementById("app").append(renderer.domElement);
 const camera = new THREE.PerspectiveCamera(40, 1, 0.1, 1000);
+camera.position.set(0, 50, 0);
+camera.up.set(0, 0, 1);
+camera.lookAt(0, 0, 0);
 const scene = new THREE.Scene();
-camera.position.z = 120;
 
-const objects =[];
+const objects = [];
 
-const sphereGeometry = new THREE.SphereBufferGeometry(1,40,40);
-const sunMaterial = new THREE.MeshPhongMaterial({emissive: 0xffff00});
+const sphereGeometry = new THREE.SphereBufferGeometry(1, 6, 6);
+const sunMaterial = new THREE.MeshPhongMaterial({ emissive: 0xffff00 });
 
 const sunMesh = new THREE.Mesh(sphereGeometry, sunMaterial);
-sunMesh.scale.set(5,5,5);
+sunMesh.scale.set(5, 5, 5);
 scene.add(sunMesh);
 
 objects.push(sunMesh);
 
-const light = new THREE.PointLight(0xFFFFFF, 3);
+const light = new THREE.PointLight(0xffffff, 3);
 scene.add(light);
 
 camera.aspect = window.innerWidth / window.innerHeight;
