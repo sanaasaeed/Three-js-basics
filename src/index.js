53365f54f7dcbb31d23356227215c610b7718ca5
Sana Saeed <41577469+sanaasaeed@users.noreply.github.com>
2021-01-02 16:15:05 +0500

    Done solar system

--- src/index.js
+++ src/index.js
@@ -1,49 +1,113 @@
 import "./styles.css";
 import * as THREE from "three";
+function main() {
+  const canvas = document.querySelector("#c");
+  const renderer = new THREE.WebGLRenderer({ canvas });
 
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.getElementById("app").append(renderer.domElement);
-const camera = new THREE.PerspectiveCamera(40, 1, 0.1, 1000);
-camera.position.set(0, 50, 0);
-camera.up.set(0, 0, 1);
-camera.lookAt(0, 0, 0);
-const scene = new THREE.Scene();
+  const fov = 40;
+  const aspect = 2; // the canvas default
+  const near = 0.1;
+  const far = 1000;
+  const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+  camera.position.set(0, 50, 0);
+  camera.up.set(0, 0, 1);
+  camera.lookAt(0, 0, 0);
 
-const objects = [];
+  const scene = new THREE.Scene();
 
-const sphereGeometry = new THREE.SphereBufferGeometry(1, 6, 6);
-const sunMaterial = new THREE.MeshPhongMaterial({ emissive: 0xffff00 });
+  {
+    const color = 0xffffff;
+    const intensity = 3;
+    const light = new THREE.PointLight(color, intensity);
+    scene.add(light);
+  }
 
-const sunMesh = new THREE.Mesh(sphereGeometry, sunMaterial);
-sunMesh.scale.set(5, 5, 5);
-scene.add(sunMesh);
+  const objects = [];
 
-objects.push(sunMesh);
+  const radius = 1;
+  const widthSegments = 6;
+  const heightSegments = 6;
+  const sphereGeometry = new THREE.SphereBufferGeometry(
+    radius,
+    widthSegments,
+    heightSegments
+  );
 
-const light = new THREE.PointLight(0xffffff, 3);
-scene.add(light);
+  const solarSystem = new THREE.Object3D();
+  scene.add(solarSystem);
+  objects.push(solarSystem);
 
-const earthMaterial = new THREE.MeshPhongMaterial({
-  color: 0x2233ff,
-  emissive: 0x112244
-});
-const earthMesh = new THREE.Mesh(sphereGeometry, earthMaterial);
-earthMesh.position.x = 10;
-scene.add(earthMesh);
-objects.push(earthMesh);
+  const sunMaterial = new THREE.MeshPhongMaterial({ emissive: 0xffff00 });
+  const sunMesh = new THREE.Mesh(sphereGeometry, sunMaterial);
+  sunMesh.scale.set(5, 5, 5);
+  solarSystem.add(sunMesh);
+  objects.push(sunMesh);
 
-function render(time) {
-  time *= 0.001;
-  objects.forEach((obj) => {
-    obj.rotation.y = time;
+  const earthMaterial = new THREE.MeshPhongMaterial({
+    color: 0x2233ff,
+    emissive: 0x112244
   });
 
-  camera.aspect = window.innerWidth / window.innerHeight;
-  camera.updateProjectionMatrix();
-  renderer.render(scene, camera);
+  const earthOrbit = new THREE.Object3D();
+  earthOrbit.position.x = 10;
+  solarSystem.add(earthOrbit);
+  objects.push(earthOrbit);
+
+  const earthMesh = new THREE.Mesh(sphereGeometry, earthMaterial);
+  earthOrbit.add(earthMesh);
+  objects.push(earthMesh);
+
+  const moonOrbit = new THREE.Object3D();
+  moonOrbit.position.x = 2;
+  earthOrbit.add(moonOrbit);
+  objects.push(moonOrbit);
+
+  const moonMaterial = new THREE.MeshPhongMaterial({
+    color: 0x888888,
+    emissive: 0x222222
+  });
+  const moonMesh = new THREE.Mesh(sphereGeometry, moonMaterial);
+  moonMesh.scale.set(0.5, 0.5, 0.5);
+  moonOrbit.add(moonMesh);
+
+  // add an AxesHelper to each node
+  objects.forEach((node) => {
+    const axes = new THREE.AxesHelper();
+    axes.material.depthTest = false;
+    axes.renderOrder = 1;
+    node.add(axes);
+  });
+
+  function resizeRendererToDisplaySize(renderer) {
+    const canvas = renderer.domElement;
+    const width = canvas.clientWidth;
+    const height = canvas.clientHeight;
+    const needResize = canvas.width !== width || canvas.height !== height;
+    if (needResize) {
+      renderer.setSize(width, height, false);
+    }
+    return needResize;
+  }
+
+  function render(time) {
+    time *= 0.001;
+
+    if (resizeRendererToDisplaySize(renderer)) {
+      const canvas = renderer.domElement;
+      camera.aspect = canvas.clientWidth / canvas.clientHeight;
+      camera.updateProjectionMatrix();
+    }
+
+    objects.forEach((obj) => {
+      obj.rotation.y = time;
+    });
+
+    renderer.render(scene, camera);
+
+    requestAnimationFrame(render);
+  }
 
   requestAnimationFrame(render);
 }
 
-requestAnimationFrame(render);
+main();
